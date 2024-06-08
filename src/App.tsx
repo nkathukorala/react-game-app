ff4ef444b611f84d26087cefec2f94985aa8d4df
Nethmi Athukorala <nethmihao2001@gmail.com>
2024-06-08 15:36:44 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -37,13 +37,14 @@ const [gameQuery,setGameQuery]=useState<GameQuery>({} as GameQuery)
         </Show>
         <GridItem area="main">
           
-          <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+          <Flex gap={5} paddingLeft={2} marginBottom={5}>
+
             <PlatformSelector
               onSelectPlatform={(platform) => setGameQuery({...gameQuery,platform})}
               selectedPlatform={gameQuery.platform}
             />
             <SortSelector selectedSortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery,sortOrder})}/>
-          </HStack>
+          </Flex>
           <GameGrid
             gameQuery={gameQuery}
           />
