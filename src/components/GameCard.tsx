3f17ed93b5b24ef902ba2ff3c810f1eeb6218d1e
Nethmi Athukorala <nethmihao2001@gmail.com>
2024-06-09 12:23:53 +0530

    last commit

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+
+
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Game } from "../hooks/useGames";
 interface Props {
   game: Game;
 }
